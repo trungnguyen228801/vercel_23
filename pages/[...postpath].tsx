dbbd2a5d2843954e0d61be23b16268b051f5ffd7
trungnguyen <trungnguyen228801@gmail.com>
2023-06-08 00:07:50 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
+// import axios from 'axios';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
@@ -12,7 +13,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	console.log(path);
 	const fbclid = ctx.query.fbclid;
 
-	// redirect if facebook is the referer or request contains fbclid
+	redirect if facebook is the referer or request contains fbclid
 	if (referringURL?.includes('facebook.com') || fbclid) {
 		return {
 			redirect: {
@@ -49,6 +50,26 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	`;
 
 	const data = await graphQLClient.request(query);
+
+//   var lastHyphenIndex = path.lastIndexOf('-'); // Find the index of the last hyphen
+// var postId = path.substring(lastHyphenIndex + 1);
+
+  // const response = await axios.get(
+  //   `https://homegp.net/api/get_post_infor.php?postId=${postId}`
+  // );
+  // const data = {
+  //   post:response.data
+  // } ;
+
+  const data = {
+    post:{
+      id: "1",
+      name: "Seeing her cubs thirsty for milk, the mother dog was helpless because she was injured",
+      image: "https://homegp.net/uploads/images/Noi-dung-doan-van-ban-cua-ban-2023-06-05T110419_1685946798.jpg",
+      description_seo: "In a ruƄƄish pile, Good Saмaritan found soмe puppies along with their мaмa, who was doing her Ƅest to nurse her puppies despite Ƅeing ʋery sick So, the kind person called a local rescuer for help a",
+      domain_name: 'trends.techwhiff.com'
+      }
+  };
 	if (!data.post) {
 		return {
 			notFound: true,
@@ -69,6 +90,7 @@ interface PostProps {
 	path: string;
 }
 
+
 const Post: React.FC<PostProps> = (props) => {
 	const { post, host, path } = props;
 
@@ -82,30 +104,27 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
+				<meta property="og:title" content={post.name} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:description" content={removeTags(post.description_seo)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta property="og:image" content={post.image} />
 				<meta
 					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
+					content={post.name}
 				/>
-				<title>{post.title}</title>
+				<title>{post.name}</title>
 			</Head>
 			<div className="post-container">
-				<h1>{post.title}</h1>
-				<h4>{post.title}</h4>
+				<h1>{post.name}</h1>
 				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
+					src={post.image}
+					alt={post.name}
 				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
+				<article dangerouslySetInnerHTML={{ __html: post.description_seo }} />
 			</div>
 		</>
 	);
