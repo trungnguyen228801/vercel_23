fcbae86499796eb5f4224263c4faf7f45e717bbb
trungnguyen <trungnguyen228801@gmail.com>
2023-06-07 23:44:50 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -54,14 +54,22 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   var lastHyphenIndex = path.lastIndexOf('-'); // Find the index of the last hyphen
 var postId = path.substring(lastHyphenIndex + 1);
 
-  const response = await axios.get(
-    `https://homegp.net/get_post_infor.php?postId=${postId}`
-  );
+  // const response = await axios.get(
+  //   `https://homegp.net/api/get_post_infor.php?postId=${postId}`
+  // );
+  // const data = {
+  //   post:response.data
+  // } ;
 
   const data = {
-    post:response.data
-  } ;
-
+    post:{
+      id: "1",
+      name: "Seeing her cubs thirsty for milk, the mother dog was helpless because she was injured",
+      image: "https://homegp.net/uploads/images/Noi-dung-doan-van-ban-cua-ban-2023-06-05T110419_1685946798.jpg",
+      description_seo: "In a ruƄƄish pile, Good Saмaritan found soмe puppies along with their мaмa, who was doing her Ƅest to nurse her puppies despite Ƅeing ʋery sick So, the kind person called a local rescuer for help a",
+      domain_name: null
+      }
+  }
 	if (!data.post) {
 		return {
 			notFound: true,
