db499f11e094b95193dc56ee9f067974a682a736
trungnguyen <trungnguyen228801@gmail.com>
2023-06-07 21:35:44 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -13,43 +13,47 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const fbclid = ctx.query.fbclid;
 
 	// redirect if facebook is the referer or request contains fbclid
-	// if (referringURL?.includes('facebook.com') || fbclid) {
-	// 	return {
-	// 		redirect: {
-	// 			permanent: false,
-	// 			destination: `${
-	// 				endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-	// 			}`,
-	// 		},
-	// 	};
-	// }
-	// var id_post = path.substring(path.lastIndexOf("-") + 1);
-	// const query = gql`
-	// 	{
-	// 		post(id:${id_post}) {
-	// 			name
-	// 			image
-	// 			description_seo
-	// 		}
-	// 	}
-	// `;
+	if (referringURL?.includes('facebook.com') || fbclid) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: `${
+					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
+				}`,
+			},
+		};
+	}
+	const query = gql`
+		{
+			post(id: "/${path}/", idType: URI) {
+				id
+				excerpt
+				title
+				link
+				dateGmt
+				modifiedGmt
+				content
+				author {
+					node {
+						name
+					}
+				}
+				featuredImage {
+					node {
+						sourceUrl
+						altText
+					}
+				}
+			}
+		}
+	`;
 
-	// const data = await graphQLClient.request(query);
-
-    const data = {
-        post:{
-            name: 'fefefef',
-            image: 'fefefefef',
-            description_seo: 'fefefwfwfacwojwgoijqowgnjoiwgn'
-        }
-    }
-
+	const data = await graphQLClient.request(query);
 	if (!data.post) {
 		return {
 			notFound: true,
 		};
 	}
-	
 	return {
 		props: {
 			path,
@@ -68,7 +72,7 @@ interface PostProps {
 const Post: React.FC<PostProps> = (props) => {
 	const { post, host, path } = props;
 
-	// to remove tags from description_seo
+	// to remove tags from excerpt
 	const removeTags = (str: string) => {
 		if (str === null || str === '') return '';
 		else str = str.toString();
@@ -78,27 +82,29 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.name} />
+				<meta property="og:title" content={post.title} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.description_seo)} />
+				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="og:image" content={post.image} />
+				<meta property="article:published_time" content={post.dateGmt} />
+				<meta property="article:modified_time" content={post.modifiedGmt} />
+				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.name}
+					content={post.featuredImage.node.altText || post.title}
 				/>
-				<title>{post.name}</title>
+				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
-				<h1>{post.name}</h1>
+				<h1>{post.title}</h1>
 				<img
-					src={post.image}
-					alt={post.name}
+					src={post.featuredImage.node.sourceUrl}
+					alt={post.featuredImage.node.altText || post.title}
 				/>
-				<article dangerouslySetInnerHTML={{ __html: post.description_seo }} />
+				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
 		</>
 	);
